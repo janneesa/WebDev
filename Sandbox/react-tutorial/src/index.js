59810ad75c309178c921898474ee5d13a6255b86
janneesa <janne.savinainen@hotmail.com>
2024-11-04 21:29:01 +0200

    tutorial watched till 3:45:00

--- Sandbox/react-tutorial/src/index.js
+++ Sandbox/react-tutorial/src/index.js
@@ -2,40 +2,43 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const firstBook = {
-  author: "Melania Trump",
-  title: "Melania",
-  img: "https://images-na.ssl-images-amazon.com/images/I/41e+dwz5W5L._AC_UL600_SR600,400_.jpg",
-};
-
-const secondBook = {
-  author: "Rebecca Yarros",
-  title: "Onyx Storm",
-  img: "https://images-na.ssl-images-amazon.com/images/I/9101MLPcFTL._AC_UL600_SR600,400_.jpg",
-};
+const books = [
+  {
+    author: "Melania Trump",
+    title: "Melania",
+    img: "https://images-na.ssl-images-amazon.com/images/I/41e+dwz5W5L._AC_UL600_SR600,400_.jpg",
+    id: 1,
+  },
+  {
+    author: "Rebecca Yarros",
+    title: "Onyx Storm",
+    img: "https://images-na.ssl-images-amazon.com/images/I/9101MLPcFTL._AC_UL600_SR600,400_.jpg",
+    id: 2,
+  },
+];
 
 const BookList = () => {
+  const getBook = (id) => {
+    const book = books.find((book) => book.id === id);
+    console.log(book);
+  };
   return (
-    <section className="bookList">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      />
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book {...book} key={book.id} getBook={getBook} />;
+      })}
     </section>
   );
 };
 
-const Book = ({ img, title, author }) => {
+const Book = (props) => {
+  const { id, img, title, author, getBook } = props;
+
   return (
     <article className="book">
       <img src={img} alt="book"></img>
       <h2>{title}</h2>
+      <button onClick={() => getBook(id)}>click me</button>
       <h4>{author}</h4>
     </article>
   );
