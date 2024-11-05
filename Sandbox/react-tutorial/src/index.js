21b8e8f5dcc301fdfcb94e947232d2797eb44405
janneesa <janne.savinainen@hotmail.com>
2024-11-05 13:50:18 +0200

    Tutorial watched till 4:08:45

--- Sandbox/react-tutorial/src/index.js
+++ Sandbox/react-tutorial/src/index.js
@@ -2,45 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const books = [
-  {
-    author: "Melania Trump",
-    title: "Melania",
-    img: "https://images-na.ssl-images-amazon.com/images/I/41e+dwz5W5L._AC_UL600_SR600,400_.jpg",
-    id: 1,
-  },
-  {
-    author: "Rebecca Yarros",
-    title: "Onyx Storm",
-    img: "https://images-na.ssl-images-amazon.com/images/I/9101MLPcFTL._AC_UL600_SR600,400_.jpg",
-    id: 2,
-  },
-];
+import { books } from "./books";
+import Book from "./Book";
 
 const BookList = () => {
-  const getBook = (id) => {
-    const book = books.find((book) => book.id === id);
-    console.log(book);
-  };
   return (
-    <section className="booklist">
-      {books.map((book) => {
-        return <Book {...book} key={book.id} getBook={getBook} />;
-      })}
-    </section>
-  );
-};
-
-const Book = (props) => {
-  const { id, img, title, author, getBook } = props;
-
-  return (
-    <article className="book">
-      <img src={img} alt="book"></img>
-      <h2>{title}</h2>
-      <button onClick={() => getBook(id)}>click me</button>
-      <h4>{author}</h4>
-    </article>
+    <>
+      <h1>Amazon best sellers</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} index={index} />;
+        })}
+      </section>
+    </>
   );
 };
 
