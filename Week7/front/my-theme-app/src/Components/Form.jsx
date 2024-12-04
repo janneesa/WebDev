cf3820b01021bca93a30fdeeb89b8b6f2533fa17
janneesa <janne.savinainen@hotmail.com>
2024-12-04 10:38:46 +0200

    done

---
+++ Week7/front/my-theme-app/src/Components/Form.jsx
@@ -0,0 +1,14 @@
+// Components/Form.jsx
+import Panel from "./Panel";
+import Button from "./Button";
+
+export default function Form() {
+  return (
+    <Panel title="Welcome to the Form">
+      {" "}
+      {/* Title of the form */}
+      <Button>Sign up</Button> {/* Button for sign-up */}
+      <Button>Log in</Button> {/* Button for log-in */}
+    </Panel>
+  );
+}
